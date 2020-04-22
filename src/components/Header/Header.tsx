2ba3cd6caeccf44a2e1cf7659c080b6b633ad255
Alec Adub <adub.alec@gmail.com>
2020-04-22 14:13:02 -0400

    Basic header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import { Button, Nav, Navbar } from 'react-bootstrap';
 import './Header.scss';
 
 const Header: React.FC = () => (
-  <div className="Header">
-    Header Component
-  </div>
+  <>
+    <Navbar bg="dark" variant="dark">
+      <Navbar.Brand href="#home"><span id="logo-title">LostPaws</span></Navbar.Brand>
+      <Nav className="mr-auto">
+      </Nav>
+      <Button id="header-button" variant="outline-primary">Report Lost Pet</Button>
+      <Button variant="outline-primary">Search</Button>
+    </Navbar>
+  </>
 );
 
 export default Header;
