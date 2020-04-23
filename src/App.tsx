dda409f115bc6d2a83a3d06fe3bf06fb59c94a27
alecadub <adub.alec@gmail.com>
2020-04-23 15:00:25 -0400

    Add modals

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,31 @@ import './App.scss';
 import AppUtilityBoxes from './components/AppUtilityBoxes/AppUtilityBoxes';
 import Header from './components/Header/Header';
 import PetCard from './components/PetCard/PetCard';
-class App extends React.Component {
+import { selectedMode } from './models/types';
 
-  public mockNumberOfCards() {
+class App extends React.Component<{}, { selectedMode: selectedMode }> {
+
+  constructor(props: any) {
+    super(props);
+    this.state = { selectedMode: 'lost' };
+    this.changeSelectedMode = this.changeSelectedMode.bind(this);
+    this.lostSelected = this.lostSelected.bind(this);
+    this.foundSelected = this.foundSelected.bind(this);
+  }
+
+  public changeSelectedMode(selectedMode: selectedMode): void {
+    this.setState({ selectedMode });
+  }
+
+  public lostSelected(): void {
+    this.changeSelectedMode('lost');
+  }
+
+  public foundSelected(): void {
+    this.changeSelectedMode('found');
+  }
+
+  public mockNumberOfCards(): number[] {
     let mockCards: number[] = [];
     for (let i: number = 0; i < 30; i++) {
       mockCards.push(1);
@@ -19,8 +41,8 @@ class App extends React.Component {
 
     return (
       <div>
-        <Header></Header>
-        <AppUtilityBoxes></AppUtilityBoxes>
+        <Header selectedMode={this.state.selectedMode} foundSelected={this.foundSelected} lostSelected={this.lostSelected}></Header>
+        <AppUtilityBoxes selectedMode={this.state.selectedMode}></AppUtilityBoxes>
         <div id="cards">
           {cards.map((result, i) => {
             return <PetCard key={i} title="The Dog" text="The most beautiful thing in the world."></PetCard>
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,16 +1,35 @@
 import React from 'react';
 import { Button, Nav, Navbar } from 'react-bootstrap';
 import './Header.scss';
-class Header extends React.Component {
+import { selectedMode } from '../../models/types';
+
+
+type headerProps = {
+  selectedMode: selectedMode,
+  lostSelected: () => void,
+  foundSelected: () => void
+}
+class Header extends React.Component<headerProps> {
+
 
   render() {
+    let lostButton;
+    let foundButton;
+    if (this.props.selectedMode === 'lost') {
+      lostButton = <Button id="header-button" variant="primary" onClick={this.props.lostSelected}>Lost</Button>;
+      foundButton = <Button id="header-button" variant="outline-primary" onClick={this.props.foundSelected}>Found</Button>;
+    } else {
+      lostButton = <Button id="header-button" variant="outline-primary" onClick={this.props.lostSelected}>Lost</Button>;
+      foundButton = <Button id="header-button" variant="primary" onClick={this.props.foundSelected}>Found</Button>;
+    }
+
     return (
       <Navbar bg="dark" variant="dark">
         <Navbar.Brand><span id="logo-title">LostPaws</span></Navbar.Brand>
         <Nav className="mr-auto">
         </Nav>
-        <Button id="header-button" variant="outline-primary">Lost</Button>
-        <Button id="header-button" variant="outline-primary">Found</Button>
+        {lostButton}
+        {foundButton}
       </Navbar>
     );
   }
