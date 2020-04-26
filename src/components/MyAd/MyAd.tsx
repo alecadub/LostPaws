16fc6a988cea986c106d02483d000b0ba80ae829
Alec Adub <adub.alec@gmail.com>
2020-04-26 13:31:42 -0400

    Fix location myAd

--- src/components/MyAd/MyAd.tsx
+++ src/components/MyAd/MyAd.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Button } from 'react-bootstrap';
 import PetCard from '../PetCardList/PetCard/PetCard';
 import './MyAd.scss';
-import Maps from '../Maps/Maps';
 
 type myAdProps = {
 }
@@ -10,11 +9,6 @@ type myAdProps = {
 class MyAd extends React.Component<myAdProps, {}> {
 
     render() {
-        let map: any;
-        if (localStorage.getItem('lat')) {
-            map = <Maps lat={localStorage.getItem('lat')} lng={localStorage.getItem('lng')}></Maps>
-        }
-
         return (
             <div id="box">
                 <div className="text">
@@ -23,13 +17,13 @@ class MyAd extends React.Component<myAdProps, {}> {
                 <PetCard key={999} imgSrc={localStorage.getItem('imgSrc')}
                     name={localStorage.getItem('petName')} type="My Pet" email={localStorage.getItem('email')}
                     animal={localStorage.getItem('animal')} breed={localStorage.getItem('breed')}
+                    lat={localStorage.getItem('lat')} lng={localStorage.getItem('lng')}
                 >
                 </PetCard>
                 <div className="buttons">
                     <Button id="header-button">Edit</Button>
                     <Button id="header-button">Delete</Button>
                 </div>
-                {map}
                 <div className="text">
                     Using AI and pet recognition technolgy, we have found the closest matches to your pet among the animals people have found and seen.
                     We also suggest looking yourself on the "Lost" tab and filtering by sightings. Refresh to get the newest data.
