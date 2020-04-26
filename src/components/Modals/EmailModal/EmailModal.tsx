dbe48c5f480d4be0e22b8d4f5acaf8f79eebce5b
Alec Adub <adub.alec@gmail.com>
2020-04-26 18:26:27 -0400

    Set email modal when lost

--- src/components/Modals/EmailModal/EmailModal.tsx
+++ src/components/Modals/EmailModal/EmailModal.tsx
@@ -1,24 +1,29 @@
 import React from 'react';
 import { Button, Form, Modal } from 'react-bootstrap';
-import './SearchModal.scss';
+import './EmailModal.scss';
+import axios from 'axios';
 
-type searchModalProps = {
+type EmailModalProps = {
     isSet: boolean,
     closeModal: () => void,
+    id: string
 }
 
-class SearchModal extends React.Component<searchModalProps, { show: boolean, valid: boolean }> {
+class EmailModal extends React.Component<EmailModalProps, { show: boolean, valid: boolean }> {
 
     private email: string | null = null;
+    private errorMsg: string | null = null;
 
     constructor(props: any) {
         super(props);
         this.state = {
-            show: false,
-            valid: false
+            show: true,
+            valid: true
         };
         this.closeModal = this.closeModal.bind(this);
         this.setEmail = this.setEmail.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.getPetById = this.getPetById.bind(this);
     }
 
     public setEmail(event: any) {
@@ -26,17 +31,49 @@ class SearchModal extends React.Component<searchModalProps, { show: boolean, val
     }
 
     public closeModal() {
-        this.setState({ show: true })
+        this.setState({ show: false });
     }
 
     public handleSubmit(event: any) {
         event.preventDefault();
+        if (this.email) {
+            this.getPetById();
+
+        }
+    }
+
+    public getPetById() {
+        let link: any = 'https://naxb0qignf.execute-api.us-east-1.amazonaws.com/dev?type=lost&id=' + this.props.id
+        console.log(link);
+        axios.get(link)
+            .then((resp: any) => {
+                if (resp.data.email === this.email) {
+                    this.errorMsg = null;
+                    this.setState({ ...this.state, show: false });
+                    localStorage.setItem('myAd', 'true');
+                    localStorage.setItem('petName', resp.data.name);
+                    localStorage.setItem('animal', resp.data.animal);
+                    localStorage.setItem('email', resp.data.email);
+                    localStorage.setItem('imgSrc', resp.data.imageUrl);
+                    localStorage.setItem('breed', resp.data.breed);
+                    localStorage.setItem('lat', resp.data.lat);
+                    localStorage.setItem('lng', resp.data.lng);
+                    this.setState({ ...this.state, show: false });
+                    this.props.closeModal();
+                } else {
+                    this.errorMsg = 'The email is valid, but doesn\'t match the email associated with the pet.';
+                    this.setState({ ...this.state, show: true });
+                }
+            })
+            .catch((resp: any) => {
+                console.error(resp);
+            })
     }
 
     render() {
 
         return (
-            <Modal show={this.props.isSet} onHide={() => this.closeModal()}>
+            <Modal show={this.props.isSet} onHide={this.props.closeModal}>
                 <Modal.Header closeButton>
                     <Modal.Title>Enter your email</Modal.Title>
                 </Modal.Header>
@@ -49,13 +86,15 @@ class SearchModal extends React.Component<searchModalProps, { show: boolean, val
                             placeholder="ex: lostpaws@gmail.com"
                             onChange={(event: any) => { this.setEmail(event) }}
                         />
+                        <p id="red">
+                            {this.errorMsg}
+                        </p>
                         <Button id="submit-button" variant="success" type="submit">Submit</Button>
                     </Form>
-
                 </Modal.Body>
             </Modal>
         );
     }
 }
 
-export default SearchModal;
+export default EmailModal;
